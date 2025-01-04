539b08ac084873f6951b45e308a1f5b140ccfd1d
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2025-01-04 12:16:19 +0800

    test: use messageId to validate test results

--- tests/unit/lint.test.ts
+++ tests/unit/lint.test.ts
@@ -18,18 +18,8 @@ describe('lint', function () {
           code: 'var boolean = true; console.info(boolean)',
           errors: [
             {
-              message:
-                'Use let or const instead of var.\n' +
-                'A variable declared with \n' +
-                'var\n' +
-                ' is accessible in the whole module. Thus, the variable can be accessed before its initialization and outside the block where it is declared.\n' +
-                'See \n' +
-                'MDN web docs\n' +
-                ' for more details.\n' +
-                "Unsafe fix: Use '\n" +
-                'const\n' +
-                "' instead.\n" +
-                'See https://biomejs.dev/linter/rules/no-var .',
+              column: 1,
+              messageId: 'lint',
             },
           ],
         },
