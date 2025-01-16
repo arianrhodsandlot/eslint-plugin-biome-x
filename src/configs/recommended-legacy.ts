92d1b5e94acbcd0b89711571af73ceba9375fd01
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2025-01-16 09:45:43 +0800

    fix: add default parserOptions for legacy config so it can work out-of-the-box

--- src/configs/recommended-legacy.ts
+++ src/configs/recommended-legacy.ts
@@ -3,6 +3,7 @@ import { pluginName } from '../constants.ts'
 import { conflicts } from './conflicts.ts'
 
 export const recommendedLegacy: Linter.LegacyConfig = {
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   plugins: [pluginName],
   rules: {
     [`${pluginName}/format`]: 'warn',
